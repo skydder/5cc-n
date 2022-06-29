94aeadadffe27ce622d051dd5e64a5834cb0e7f0
skydder <skydder.shoma@gmail.com>
2022-06-29 14:04:51 +0900

    Made compiler that can add and sub

--- src/5cc.c
+++ src/5cc.c
@@ -5,6 +5,7 @@
 void Error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+    fprintf(stderr, "[ERROR] ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -22,11 +23,28 @@ int main(int argc, char **argv) {
         Error("引数の個数が正しくありません");
         return 1;
     }
-    
+    char *p = argv[1];
+
     println(".intel_syntax noprefix");
     println(".globl main");
     println("main:");
-    println("\tmov rax, %d", atoi(argv[1]));
+    println("\tmov rax, %ld", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            println("\tadd rax, %ld", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            println("\tsub rax, %ld", strtol(p, &p, 10));
+            continue; 
+        }
+
+        Error("Unexpected letter: '%c'", *p);
+    }
     println("\tret");
     
     return 0;
