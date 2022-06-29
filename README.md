9861b0dc7f8656f2144142c8e5c2afaff9d90b5a
skydder <skydder.shoma@gmail.com>
2022-06-29 20:27:20 +0900

    Add tokenizer

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 This is a practice repo of [C Compiler Book](https://www.sigbus.info/compilerbook)
 
-##Reference
+## Reference
 [pocari/compilerbook-9cc](https://github.com/pocari/compilerbook-9cc)
 [rui314/chibicc](https://github.com/rui314/chibicc)
 [rui314/9cc](https://github.com/rui314/9cc)
--- src/5cc.c
+++ src/5cc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 void Error(char *fmt, ...) {
     va_list ap;
@@ -18,32 +21,102 @@ void println(char *fmt, ...) {
     return ;
 }
 
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+Token *token;
+
+Token *NewToken(Token *cur, TokenKind TK, char *str) {
+    Token *new = calloc(sizeof(Token), 1);
+    new->kind = TK;
+    new->str = str;
+    cur->next = new;
+    return new;
+}
+bool ConsumeToken(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+void ExpectToken(char op) {
+    if (!ConsumeToken(op)) {
+         Error("It's not '%c'", op);
+    }
+}
+
+int ExpectTokenNum() {
+    if (token->kind != TK_NUM)
+        Error("数ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool IsTokenAtEof() {
+  return token->kind == TK_EOF;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+        if (*p =='+' || *p == '-') {
+            cur = NewToken(cur, TK_RESERVED, p);
+            p++;
+            continue;
+        }
+        if (isdigit(*p)) {
+            cur = NewToken(cur, TK_NUM, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        Error("Can't tokenize!");
+    }
+
+    NewToken(cur, TK_EOF, p);
+    return head.next;
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         Error("引数の個数が正しくありません");
         return 1;
     }
-    char *p = argv[1];
+    token = tokenize(argv[1]);
 
     println(".intel_syntax noprefix");
     println(".globl main");
     println("main:");
-    println("\tmov rax, %ld", strtol(p, &p, 10));
-
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            println("\tadd rax, %ld", strtol(p, &p, 10));
+    println("\tmov rax, %d", ExpectTokenNum());
+    while (!IsTokenAtEof()) {
+        if (ConsumeToken('+')) {
+            println("\tadd rax, %d", ExpectTokenNum());
             continue;
         }
-
-        if (*p == '-') {
-            p++;
-            println("\tsub rax, %ld", strtol(p, &p, 10));
-            continue; 
+        if (ConsumeToken('-')) {
+            println("\tsub rax, %d", ExpectTokenNum());
+            continue;
         }
-
-        Error("Unexpected letter: '%c'", *p);
+        Error("something is wrong");
     }
     println("\tret");
     
