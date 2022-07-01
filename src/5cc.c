1d764360ca484a525fabf81335ce461cdb683b6f
skydder <skydder.shoma@gmail.com>
2022-07-01 18:07:51 +0900

    Mock Chibicc(a1ab0ff26f23c82f15180051204eeb6279747c9a)

--- src/5cc.c
+++ src/5cc.c
@@ -34,39 +34,48 @@ struct Token {
     Token *next;
     int val;
     char *str;
+    int len;
 };
-Token *token;
 
-Token *NewToken(Token *cur, TokenKind TK, char *str) {
+Token *NewToken(TokenKind TK, char *start, char *end) {
     Token *new = calloc(sizeof(Token), 1);
     new->kind = TK;
-    new->str = str;
-    cur->next = new;
+    new->str = start;
+    new->len = end - start;
     return new;
 }
-bool ConsumeToken(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op) {
+bool ConsumeToken(Token *tok, char op) {
+    if (tok->kind != TK_RESERVED || tok->str[0] != op) {
         return false;
     }
-    token = token->next;
+    tok = tok->next;
     return true;
 }
-void ExpectToken(char op) {
-    if (!ConsumeToken(op)) {
+void ExpectToken(Token *tok,char op) {
+    if (!ConsumeToken(tok, op)) {
          Error("It's not '%c'", op);
     }
 }
+bool IsTokenEqual(Token *tok, char *op) {
+  return memcmp(tok->str, op, tok->len) == 0 && op[tok->len] == '\0';
+}
 
-int ExpectTokenNum() {
-    if (token->kind != TK_NUM)
+Token *SkipToken(Token *tok, char *s) {
+  if (!IsTokenEqual(tok, s))
+    Error("expected '%s'", s);
+  return tok->next;
+}
+
+int ExpectTokenNum(Token **tok) {
+    if ((*tok)->kind != TK_NUM)
         Error("数ではありません");
-    int val = token->val;
-    token = token->next;
+    int val = (*tok)->val;
+    *tok = (*tok)->next;
     return val;
 }
 
-bool IsTokenAtEof() {
-  return token->kind == TK_EOF;
+bool IsTokenAtEof(Token *tok) {
+  return tok->kind == TK_EOF;
 }
 
 Token *tokenize(char *p) {
@@ -80,19 +89,21 @@ Token *tokenize(char *p) {
             continue;
         }
         if (*p =='+' || *p == '-') {
-            cur = NewToken(cur, TK_RESERVED, p);
+            cur = cur->next = NewToken(TK_RESERVED, p, p + 1);
             p++;
             continue;
         }
         if (isdigit(*p)) {
-            cur = NewToken(cur, TK_NUM, p);
+            cur = cur->next = NewToken(TK_NUM, p, p);
+            char *q = p;
             cur->val = strtol(p, &p, 10);
+            cur->len = p - q;
             continue;
         }
         Error("Can't tokenize!");
     }
 
-    NewToken(cur, TK_EOF, p);
+    cur = cur->next = NewToken(TK_EOF, p, p);
     return head.next;
 }
 
@@ -101,21 +112,26 @@ int main(int argc, char **argv) {
         Error("引数の個数が正しくありません");
         return 1;
     }
-    token = tokenize(argv[1]);
+    Token *token = tokenize(argv[1]);
 
     println(".intel_syntax noprefix");
     println(".globl main");
     println("main:");
-    println("\tmov rax, %d", ExpectTokenNum());
-    while (!IsTokenAtEof()) {
-        if (ConsumeToken('+')) {
-            println("\tadd rax, %d", ExpectTokenNum());
+    println("\tmov rax, %d", ExpectTokenNum(&token));
+    //token = token->next;
+
+    while (!IsTokenAtEof(token)) {
+        if (IsTokenEqual(token, "+")) {
+            token = token->next;
+            println("\tadd rax, %d", ExpectTokenNum(&token));
             continue;
         }
-        if (ConsumeToken('-')) {
-            println("\tsub rax, %d", ExpectTokenNum());
+        if (IsTokenEqual(token, "+")) {
+            token = token->next;
+            println("\tsub rax, %d", ExpectTokenNum(&token));
             continue;
         }
+
         Error("something is wrong");
     }
     println("\tret");
